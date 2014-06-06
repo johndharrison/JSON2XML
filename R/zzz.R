606cd063222c8d2e9f1814d863bdad41e36a03b3
johndharrison <johndharrison0@gmail.com>
2014-06-06 20:44:08 +0700

    Ammend zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -3,13 +3,11 @@ require(rJava)
 .jpackage(name = 'JSON2XML', jars = "*")
 
 .onLoad <- function(libname, pkgname) {
-  # load the jsoup jar
   library(rJava)
   .jpackage(name = pkgname, jars = "*")
 }
 
 .onAttach <- function(libname, pkgname) {
-  # load the jsoup jar
   library(rJava)
   .jpackage(name = pkgname, jars = "*")
 }
